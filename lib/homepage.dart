9594c1aa21609adccf0ec0a07ade6b838f8e01ce
Moises David Higuerey Hernandez <hhmoises@hotmail.com>
2020-03-15 19:39:11 -0600

    [New] Bar chart + other test charts added
    Signed-off-by: Moises David Higuerey Hernandez <hhmoises@hotmail.com>

---
+++ lib/homepage.dart
@@ -0,0 +1,311 @@
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  final Widget child;
+
+  HomePage({Key key, this.child}) : super(key: key);
+
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<charts.Series<DisBarra, String>> _seriesData;
+  List<charts.Series<Task, String>> _seriesPieData;
+  List<charts.Series<Sales, int>> _seriesLineData;
+
+  _generateData() {
+    final hombresData = [
+      new DisBarra('Vision', 112613),
+      new DisBarra('Oido', 37240),
+      new DisBarra('Voz', 16595),
+      new DisBarra('Caminar', 64271),
+      new DisBarra('Brazos', 23001),
+      new DisBarra('Intelecto', 19968),
+      new DisBarra('Mente', 14126),
+    ];
+
+    final mujeresData = [
+      new DisBarra('Vision', 138851),
+      new DisBarra('Oido', 33469),
+      new DisBarra('Voz', 12818),
+      new DisBarra('Caminar', 76109),
+      new DisBarra('Brazos', 25858),
+      new DisBarra('Intelecto', 15448),
+      new DisBarra('Mente', 12845),
+    ];
+
+    var piedata = [
+      new Task('Work', 35.8, Color(0xff3366cc)),
+      new Task('Eat', 8.3, Color(0xff990099)),
+      new Task('Commute', 10.8, Color(0xff109618)),
+      new Task('TV', 15.6, Color(0xfffdbe19)),
+      new Task('Sleep', 19.2, Color(0xffff9900)),
+      new Task('Other', 10.3, Color(0xffdc3912)),
+    ];
+
+    var linesalesdata = [
+      new Sales(0, 45),
+      new Sales(1, 56),
+      new Sales(2, 55),
+      new Sales(3, 60),
+      new Sales(4, 61),
+      new Sales(5, 70),
+    ];
+    var linesalesdata1 = [
+      new Sales(0, 35),
+      new Sales(1, 46),
+      new Sales(2, 45),
+      new Sales(3, 50),
+      new Sales(4, 51),
+      new Sales(5, 60),
+    ];
+
+    var linesalesdata2 = [
+      new Sales(0, 20),
+      new Sales(1, 24),
+      new Sales(2, 25),
+      new Sales(3, 40),
+      new Sales(4, 45),
+      new Sales(5, 60),
+    ];
+
+    _seriesData.add(
+      charts.Series(
+        domainFn: (DisBarra disbarra, _) => disbarra.discapacidad,
+        measureFn: (DisBarra disbarra, _) => disbarra.cantidad,
+        id: 'Hombres',
+        data: hombresData,
+        fillPatternFn: (_, __) => charts.FillPatternType.solid,
+        fillColorFn: (DisBarra disbarra, _) =>
+            charts.ColorUtil.fromDartColor(Color(0xff2296F3)),
+      ), 
+    );
+
+    _seriesData.add(
+      charts.Series(
+        domainFn: (DisBarra disbarra, _) => disbarra.discapacidad,
+        measureFn: (DisBarra disbarra, _) => disbarra.cantidad,
+        id: 'Mujeres',
+        data: mujeresData,
+        fillPatternFn: (_, __) => charts.FillPatternType.solid,
+        fillColorFn: (DisBarra disbarra, _) =>
+           charts.ColorUtil.fromDartColor(Color(0xff90CBF9)),
+      ),
+    );
+    _seriesPieData.add(
+      charts.Series(
+        domainFn: (Task task, _) => task.task,
+        measureFn: (Task task, _) => task.taskvalue,
+        colorFn: (Task task, _) =>
+            charts.ColorUtil.fromDartColor(task.colorval),
+        id: 'Air Pollution',
+        data: piedata,
+         labelAccessorFn: (Task row, _) => '${row.taskvalue}',
+      ),
+    );
+
+    _seriesLineData.add(
+      charts.Series(
+        colorFn: (__, _) => charts.ColorUtil.fromDartColor(Color(0xff990099)),
+        id: 'Air Pollution',
+        data: linesalesdata,
+        domainFn: (Sales sales, _) => sales.yearval,
+        measureFn: (Sales sales, _) => sales.salesval,
+      ),
+    );
+    _seriesLineData.add(
+      charts.Series(
+        colorFn: (__, _) => charts.ColorUtil.fromDartColor(Color(0xff109618)),
+        id: 'Air Pollution',
+        data: linesalesdata1,
+        domainFn: (Sales sales, _) => sales.yearval,
+        measureFn: (Sales sales, _) => sales.salesval,
+      ),
+    );
+    _seriesLineData.add(
+      charts.Series(
+        colorFn: (__, _) => charts.ColorUtil.fromDartColor(Color(0xffff9900)),
+        id: 'Air Pollution',
+        data: linesalesdata2,
+        domainFn: (Sales sales, _) => sales.yearval,
+        measureFn: (Sales sales, _) => sales.salesval,
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _seriesData = List<charts.Series<DisBarra, String>>();
+    _seriesPieData = List<charts.Series<Task, String>>();
+    _seriesLineData = List<charts.Series<Sales, int>>();
+    _generateData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color(0xff1976d2),
+            //backgroundColor: Color(0xff308e1c),
+            bottom: TabBar(
+              indicatorColor: Color(0xff9962D0),
+              tabs: [
+                Tab(
+                  icon: Icon(FontAwesomeIcons.solidChartBar),
+                ),
+                Tab(icon: Icon(FontAwesomeIcons.chartPie)),
+                Tab(icon: Icon(FontAwesomeIcons.chartLine)),
+              ],
+            ),
+            title: Text('Flutter Charts'),
+          ),
+          body: TabBarView(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Container(
+                  child: Center(
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                            'Discapacidad en Costa Rica por tipo',style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),),
+                        Expanded(
+                          child: charts.BarChart(
+                            _seriesData,
+                            animate: false,
+                            barGroupingType: charts.BarGroupingType.grouped,
+                            behaviors: [new charts.SeriesLegend(outsideJustification: charts.OutsideJustification.endDrawArea,
+                              horizontalFirst: false,
+                              desiredMaxRows: 2,
+                              cellPadding: new EdgeInsets.only(right: 4.0, bottom: 4.0),
+                              entryTextStyle: charts.TextStyleSpec(
+                                  color: charts.MaterialPalette.black,
+                                  fontFamily: 'Georgia',
+                                  fontSize: 11),
+                            )
+                            ],
+
+                            animationDuration: Duration(seconds: 5),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Container(
+                  child: Center(
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                            'Time spent on daily tasks',style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),),
+                            SizedBox(height: 10.0,),
+                        Expanded(
+                          child: charts.PieChart(
+                            _seriesPieData,
+                            animate: true,
+                            animationDuration: Duration(seconds: 5),
+                             behaviors: [
+                            new charts.DatumLegend(
+                              outsideJustification: charts.OutsideJustification.endDrawArea,
+                              horizontalFirst: false,
+                              desiredMaxRows: 2,
+                              cellPadding: new EdgeInsets.only(right: 4.0, bottom: 4.0),
+                              entryTextStyle: charts.TextStyleSpec(
+                                  color: charts.MaterialPalette.purple.shadeDefault,
+                                  fontFamily: 'Georgia',
+                                  fontSize: 11),
+                            )
+                          ],
+                           defaultRenderer: new charts.ArcRendererConfig(
+                              arcWidth: 100,
+                             arcRendererDecorators: [
+          new charts.ArcLabelDecorator(
+              labelPosition: charts.ArcLabelPosition.inside)
+        ])),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Container(
+                  child: Center(
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                            'Sales for the first 5 years',style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),),
+                        Expanded(
+                          child: charts.LineChart(
+                            _seriesLineData,
+                            defaultRenderer: new charts.LineRendererConfig(
+                                includeArea: true, stacked: true),
+                            animate: true,
+                            animationDuration: Duration(seconds: 5),
+                            behaviors: [
+        new charts.ChartTitle('Years',
+            behaviorPosition: charts.BehaviorPosition.bottom,
+            titleOutsideJustification:charts.OutsideJustification.middleDrawArea),
+        new charts.ChartTitle('Sales',
+            behaviorPosition: charts.BehaviorPosition.start,
+            titleOutsideJustification: charts.OutsideJustification.middleDrawArea),
+        new charts.ChartTitle('Departments',
+            behaviorPosition: charts.BehaviorPosition.end,
+            titleOutsideJustification:charts.OutsideJustification.middleDrawArea,
+            )   
+      ]
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Pollution {
+  String place;
+  int year;
+  int quantity;
+
+  Pollution(this.year, this.place, this.quantity);
+}
+
+class Task {
+  String task;
+  double taskvalue;
+  Color colorval;
+
+  Task(this.task, this.taskvalue, this.colorval);
+}
+
+class Sales {
+  int yearval;
+  int salesval;
+
+  Sales(this.yearval, this.salesval);
+}
+
+class DisBarra {
+  final String discapacidad;
+  final int cantidad;
+
+  DisBarra(this.discapacidad, this.cantidad);
+}
